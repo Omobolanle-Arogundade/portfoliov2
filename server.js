b8dadf941e4a2ce62ca6d2d2c6bf3fe6edd2220d
Bolanle Bincom <bolanle.bincom@gmail.com>
2019-08-06 17:31:21 +0100

    updated app

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/portfoliov2'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/portfoliov2/index.html'));
+res.sendFile(path.join(__dirname + '/dist/portfoliov2/index.html'));
 });
 
 // Start the app by listening on the default Heroku port

ba193df01fc963f58764e366b8c79749be352815
Bolanle Bincom <bolanle.bincom@gmail.com>
2019-08-06 17:17:45 +0100

    updated server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/portfoliov2'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
 

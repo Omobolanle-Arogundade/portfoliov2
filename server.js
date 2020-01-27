ab8167b6e28d0eb294dece7ed99119672457ab9c
Omobolanle Aro <omobolanlearogundade@gmail.com>
2020-01-27 07:53:48 +0100

    updated the CV file

--- server.js
+++ server.js
@@ -13,5 +13,6 @@ app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname + '/dist/portfoliov2/index.html'));
 });
 
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
